cbde4ecec8ee384bb513bb6d2b4c200c2de2731a
Aditya Phasu <adityaphasu919@gmail.com>
2025-02-05 00:05:09 +0530

    add navigation between Grants and FAQ sections in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
 import Board from "./components/Board";
+import FAQ from "./components/FAQ";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("Grants");
+
+  const render = () => {
+    switch (currentPage) {
+      case "Grants":
+        return <Board />;
+      case "FAQ":
+        return <FAQ />;
+      default:
+        return <Board />;
+    }
+  };
+
   return (
     <main className="flex flex-col items-center justify-center py-20">
       <div className="flex flex-col items-center space-y-4">
@@ -12,7 +27,23 @@ function App() {
           opportunities.
         </p>
       </div>
-      <Board />
+      <div className="w-[80%] mx-auto my-6">
+        <div className="flex items-center pt-3 gap-5 font-medium text-white mb-0.5 border-b border-gray-600">
+          <button className="relative" onClick={() => setCurrentPage("Grants")}>
+            Search Grants
+            {currentPage === "Grants" && (
+              <span className="w-full absolute bg-[#00bbfc] h-0.5 left-0 bottom-0"></span>
+            )}
+          </button>
+          <button className="relative" onClick={() => setCurrentPage("FAQ")}>
+            FAQ
+            {currentPage === "FAQ" && (
+              <span className="w-full absolute bg-[#00bbfc] h-0.5 left-0 bottom-0"></span>
+            )}
+          </button>
+        </div>
+        {render()}
+      </div>
       <div className="flex flex-col items-center space-y-4">
         <div className="w-[14.625rem] my-8">
           <a href="https://www.cornarolabs.xyz">
